dce63141499a7ff1dabbcfd4dd19f31f8c79f8c3
Matteo <matteo.iorio01@gmail.com>
2022-04-26 22:25:22 +0200

    Init of the product

---
+++ Iorio/Product.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace unieuroopCSharp1
+{
+    class Product
+    {
+        public enum Category
+        {
+
+        }
+    }
+}
