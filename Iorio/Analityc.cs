951f80671e09a9bf8148bd7e18db3825870ede29
Matteo <matteo.iorio01@gmail.com>
2022-04-26 22:34:01 +0200

    Adding more methods inside Analytic

--- Iorio/Analityc.cs
+++ Iorio/Analityc.cs
@@ -10,6 +10,13 @@ namespace unieuroopCSharp1
         HashSet<Product> getTotalProductsSold();
         int getQuantitySoldOf(Product product);
         int getQuantitySoldOf(Product product, Predicate<DateTime> date);
+        Dictionary<Product, int> getOrderedByCategory(Predicate<Product.Category> categories);
+        HashSet<Product> getProductByDate(Predicate<DateTime> date);
+        Dictionary<DateTime, int> getSoldOnDay(Predicate<DateTime> datePredicate);
+        Dictionary<Product.Category, int> getCategoriesSold();
+        Dictionary<int, Double> getTotalSpentByYear();
+        Dictionary<int, Double> getTotalEarnedByYear();
+        Dictionary<DateTime.Month, Double> getTotalEarnedByMonth(Predicate<int> year);
 
     }
 }
