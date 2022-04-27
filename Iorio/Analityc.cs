1b77412e8bd046b14f271ab4dd48c87c541fb40d
Matteo <matteo.iorio01@gmail.com>
2022-04-27 21:40:34 +0200

    Adding more methods in Analytic

--- Iorio/Analityc.cs
+++ Iorio/Analityc.cs
@@ -7,16 +7,75 @@ namespace unieuroopCSharp1
 {
     interface Analityc
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         HashSet<Product> getTotalProductsSold();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         int getQuantitySoldOf(Product product);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         int getQuantitySoldOf(Product product, Predicate<DateTime> date);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         Dictionary<Product, int> getOrderedByCategory(Predicate<Product.Category> categories);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         HashSet<Product> getProductByDate(Predicate<DateTime> date);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         Dictionary<DateTime, int> getSoldOnDay(Predicate<DateTime> datePredicate);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         Dictionary<Product.Category, int> getCategoriesSold();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         Dictionary<int, Double> getTotalSpentByYear();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         Dictionary<int, Double> getTotalEarnedByYear();
-        Dictionary<DateTime.Month, Double> getTotalEarnedByMonth(Predicate<int> year);
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        Dictionary<DateTime, Double> getTotalEarnedByMonth(Predicate<int> year);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        Dictionary<DateTime, Double> getTotalSpentByMonth(Predicate<int> year);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        double getTotalStockPrice();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        double getTotalShopEarned();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        double getTotalAmountSpent();
     }
 }
