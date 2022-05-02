098611476c6678884a133cfcdb1d00f47a643c24
Samuele Ferri <samuele1301@gmail.com>
2022-05-02 10:45:03 +0200

    implementations of client class

--- unieuroopSharp/Ferri/IClient.cs
+++ unieuroopSharp/Ferri/IClient.cs
@@ -1,8 +1,36 @@
 ﻿using System;
+
 namespace unieuroopSharp.Ferri
 {
 	public interface IClient
 	{
-	}
+        /// <summary>
+		/// This method is used to get the base person.
+		/// </summary>
+		/// <param></param>
+		/// <returns> base person</returns>
+		BasePerson GetPerson();
+
+        /// <summary>
+        /// This method is used to override toStrig.
+        /// </summary>
+        /// <param></param>
+        /// <returns> the string of the client </returns>
+        string toString();
+
+        /// <summary>
+        /// This method is used to override hashCode.
+        /// </summary>
+        /// <param></param>
+        /// <returns> hashCode for client </returns>
+        int hashCode();
+
+        /// <summary>
+        /// This method is used to override equals.
+        /// </summary>
+        /// <param></param>
+        /// <returns> equals for the client </returns>
+        bool equals(Object obj);
+    }
 }
 
--- unieuroopSharp/Ferri/IStaff.cs
+++ unieuroopSharp/Ferri/IStaff.cs
@@ -3,6 +3,12 @@ namespace unieuroopSharp.Ferri
 {
 	public interface IStaff
 	{
+		/// <summary>
+		/// This method is used to get the base person.
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns> base person</returns>
+		void SetEmail(string email);
 	}
 }
 
