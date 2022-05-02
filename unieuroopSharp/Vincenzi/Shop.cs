14e4b7cbd5b8ce5c98f099e058570052771912b5
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-05-02 08:07:56 +0200

    adding properties in shop and implementing some methods

--- unieuroopSharp/Vincenzi/Shop.cs
+++ unieuroopSharp/Vincenzi/Shop.cs
@@ -7,24 +7,52 @@ namespace unieuroopSharp.Vincenzi
 {
     class Shop : IShop
     {
+        public HashSet<Department> Departments { get; private set; }
+        public HashSet<Staff> Staffs { get; private set; }
+        public HashSet<Supplier> Suppliers { get; private set; }
+        public HashSet<Sale> Sales { get; private set; }
+        public HashSet<Client> RegisteredClients { get; private set; }
+        public Stock Stock { get; private set; }
+        public Dictionary<LocalDate, Double> Bills { get; private set; }
+        public string Name { get; set; }
+        public Shop(string name, HashSet<Department> departments,
+                HashSet<Staff> staffs, HashSet<Supplier> suppliers,
+                HashSet<Sale> sales, HashSet<Client> registeredClients,
+                Stock stock, Dictionary<LocalDate, Double> bills)
+        {
+
+        public Shop(string name)
+        {
+            this(name, new HashSet<>(), new HashSet<>(), new HashSet<>(), new HashSet<>(), new HashSet<>(), new StockImpl(), new HashMap<>());
+        }
+
+            this.Name = name;
+            this.Departments = departments;
+            this.Staffs = staffs;
+            this.Suppliers = suppliers;
+            this.Sales = sales;
+            this.RegisteredClients = registeredClients;
+            this.Stock = stock;
+            this.Bills = bills;
+        }
         public void AddBills(DateTime date, double spent)
         {
-            throw new NotImplementedException();
+            this.Bills.Add(spent, date);
         }
 
         public void AddDepartment(Department department)
         {
-            throw new NotImplementedException();
+            this.Departments.Add(department);
         }
 
         public void AddSale(Sale sale)
         {
-            throw new NotImplementedException();
+            this.Sales.Add(sale);
         }
 
         public void AddStaff(Staff staff)
         {
-            throw new NotImplementedException();
+            this.Staffs.Add(staff);
         }
 
         public void AddStaffIn(Department departmentInput, HashSet<Staff> staff)
@@ -34,7 +62,7 @@ namespace unieuroopSharp.Vincenzi
 
         public void AddSupplier(Supplier supplier)
         {
-            throw new NotImplementedException();
+            this.Suppliers.Add(supplier);
         }
 
         public void EditClient(string name, string surname, DateTime birthday, Client client)
@@ -64,7 +92,7 @@ namespace unieuroopSharp.Vincenzi
 
         public void RegisterClient(Client client)
         {
-            throw new NotImplementedException();
+            this.RegisteredClients.Add(client);
         }
 
         public void RemoveClient(Client client)
@@ -74,7 +102,10 @@ namespace unieuroopSharp.Vincenzi
 
         public void RemoveDepartment(Department department)
         {
-            throw new NotImplementedException();
+            if (!this.Departments.Remove(department))
+            {
+                throw new NoSuchElementException("The input department does not exist");
+            }
         }
 
         public void RemoveSale(Sale sale)
