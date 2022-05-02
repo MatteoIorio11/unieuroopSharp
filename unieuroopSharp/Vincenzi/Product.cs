4f130424f48a3b5969b26fe95ef342044d30a696
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-05-02 16:09:41 +0200

    adding enum category

--- unieuroopSharp/Vincenzi/Product.cs
+++ unieuroopSharp/Vincenzi/Product.cs
@@ -6,5 +6,28 @@ namespace unieuroopSharp.Vincenzi
 {
     class Product
     {
+        public int ProductCode { get; private set; }
+        public double PurchasePrice { get; private set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        private double SellingPrice { get; set; }
+        public string Description { get; set; }
+        public Category ProductCategory { get; set; }
+        public enum Category
+        {
+            SMARTPHONE,PC,HOME,SMARTWATCH,DOMESTIC_APPLIANCE,
+            COMPUTER,
+            PC_DESKTOP,
+            MONITOR,    
+            KEYBOARD,    
+            MOUSE,
+            SCANNER,
+            PRINTER,
+            NAS,
+            HARD_DISK_AND_STORAGE,
+            NETWORKING,
+            VIDEO_SURVEILLANCE,
+            TABLET
+        }
     }
 }
