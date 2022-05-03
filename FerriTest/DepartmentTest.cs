ae0cf07c7314ab54dba4f1926d40d0762be94506
Samuele Ferri <samuele1301@gmail.com>
2022-05-03 11:55:36 +0200

    add FerriTest

---
+++ FerriTest/DepartmentTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using unieuroopSharp.Vincenzi;
+
+namespace FerriTest
+{
+	public class DepartmentTest
+	{
+		public DepartmentTest()
+		{
+		}
+	}
+}
+
