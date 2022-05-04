6199f7e98004854929eeac302ab20b91f0bfdf5e
nicola_strada <nicolastrada5@gmail.com>
2022-05-04 12:03:17 +0200

    Init of Supplier

---
+++ unieuroopSharp/Strada/Supplier.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using unieuroopSharp.Strada;
+using unieuroopSharp.Vincenzi;
+
+namespace unieuroopSharp.Strada
+{
+    public class Supplier : ISupplier
+    {
+        
+    }
+}
