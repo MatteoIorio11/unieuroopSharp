7dfa7c6ffef234d200b94752925e0896ae7e3f7d
nicola_strada <nicolastrada5@gmail.com>
2022-05-04 12:37:16 +0200

    Implementation of methods in Supplier

--- unieuroopSharp/Strada/Supplier.cs
+++ unieuroopSharp/Strada/Supplier.cs
@@ -9,6 +9,51 @@ namespace unieuroopSharp.Strada
 {
     public class Supplier : ISupplier
     {
-        
+        public Dictionary<Product, double> SalableProducts { get; private set; }
+        public string Name { get; private set; }
+
+        public Supplier(string name, Dictionary<Product, double> catalog)
+        {
+            this.SalableProducts = catalog;
+            this.Name = name;
+        }
+
+        double GetPriceOf(Product product, int amount)
+        {
+            if(this.SalableProducts.ContainsKey(product))
+            {
+                return this.SalableProducts[product] * amount;
+            }
+            else
+            {
+                throw new ArgumentException("The product is not saled from this Supplier");
+            }
+        }
+
+        double GetTotalPriceByProducts(Dictionary<Product, int> productsPurchased)
+        {
+            double totalePrice = 0;
+            foreach (Product product in productsPurchased)
+            {
+                if(!this.SalableProducts.ContainsKey(product))
+                {
+                    throw new ArgumentException("Some of the products purchased are not saled from this Supplier");
+                }
+                totalePrice += this.SalableProducts[product] * productsPurchased[product];
+            }
+            return totalePrice;
+        }
+
+        Dictionary<Product, int> SellProduct(Dictionary<Product, int> productsPurchased)
+        {
+            foreach (Product product in productsPurchased)
+            {
+                if(!this.SalableProducts.ContainsKey(product))
+                {
+                    throw new ArgumentException("Some of the products purchased are not saled from this Supplier");
+                }
+            }
+            return productsPurchased;
+        }
     }
 }
